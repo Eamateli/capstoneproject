d900ca13c56803d1a09c5d1dfc62a97fa9e95073
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-07-01 21:36:11 +0300

    Done

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero"
+import Menu from "../components/Menu"
+import Testimonials from "../components/Testimonials"
+
+export default function HomePage() {
+    return (
+      <main>
+       <Hero />
+       <Menu />
+       <Testimonials />
+      </main>
+    )
+  }
+  
+
